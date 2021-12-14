6a4abaf60ae5ae95c47639590be0239aad5f40cb
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-12-14 12:10:30 -0800

    Completed Assessment with class

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,16 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+// const { expect } = require("@jest/globals");
+
+// const { describe } = require("yargs")
+
+// describe("", () => {
+//   it("", () => {
+//     expect().toEqual();
+//     expect().toEqual();
+//     expect().toEqual();
+//   });
+// });
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,45 +26,142 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
-// Expected output: "15 is divisible by three"
-var num2 = 0
-// Expected output: "0 is divisible by three"
-var num3 = -7
-// Expected output: "-7 is not divisible by three"
-
-
-
+describe("evenlyDivisibleByThree", ()=>{
+    // Following the example in the syllabus
+    // declaring variables inside the test to use as inputs in the test expect statements
+    var num1 = 15
+    // Expected output: "15 is divisible by three"
+    var num2 = 0
+    // Expected output: "0 is divisible by three"
+    var num3 = -7
+    // Expected output: "-7 is not divisible by three"
+    it("use the tool modulus to see if the remainder goes into 3 evenly", ()=>{
+        expect(evenlyDivisibleByThree(num1)).toEqual("15 is divisible by three");
+        expect(evenlyDivisibleByThree(num2)).toEqual("0 is divisible by three");
+        expect(evenlyDivisibleByThree(num3)).toEqual("-7 is not divisible by three");
+    });
+})
+// RED
 // b) Create the function that makes the test pass.
 
+// Pseudo Code: Create a function that takes a number (num) as an argument
+// use the tool modulus to see if the remainder goes into 3 evenly
+    // check if the argument passed through is not evenly divided by three
+    // return num is not divisible by three
+    // else return num is divisible by three
+// return a string literal becuase we need the variable that we are pssing in to the function in the return statement 
 
+// Refactor code using ternary opperator instead of conditional statments. Ternary is a more apporpriate tool for two conditionals. It looks hella cool.
+
+// const evenlyDivisibleByThree = (num) => {
+//     if (num % 3 !== 0) {
+//         return `${num} is not divisible by three`;
+//     } else {
+//         return `${num} is divisible by three`
+//     }
+// } 
+// GREEN
+const evenlyDivisibleByThree = (num) => {
+    return num % 3 !== 0 ? `${num} is not divisible by three` : `${num} is divisible by three`;
+}; 
+// Refactor
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+// Pseudo Code: for the expect take in the array and .toEqual and capitalized first letter of every string
 
 
+describe("capitalized", () => {
+    var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+    // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+    var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+    // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+  it("a function that takes in an array of words and returns an array with all the words capitalized.", () => {
+    expect(capitalized(randomNouns1)).toEqual([
+      "Streetlamp",
+      "Potato",
+      "Teeth",
+      "Conclusion",
+      "Nephew",
+    ]);
+    expect(capitalized(randomNouns2)).toEqual([
+      "Temperature",
+      "Database",
+      "Chopsticks",
+      "Mango",
+      "Deduction",
+    ]);
+  });
+});
+//RED  Test 2 failed   ReferenceError: capitalized is not defined
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code: Create the function capitalized w/ the parameter of array
+// define a new variable that will take in (array) and itterate through it's values via .map() 
+    // return the value at the first index with .toUpperCase(), used the .substring() using the argument 1  to complete each string with lower case letters
+// return variable
+// run test
+// refactor psuedo code : remove variable and return array.map() 
 
+
+// const capitalized = (array) => {
+//     let firstCap = array.map(value => {
+//         return value[0].toUpperCase() + value.substring(1)
+//     })
+//     return firstCap
+// }
+// Green
+
+const capitalized = (array) => {
+  return array.map((value) => value[0].toUpperCase() + value.substring(1));
+};
+// const capitalized = (array) => {
+//   return array.map((value) => value[0].toUpperCase() + value.slice(1));
+// };
+// refactor
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+// Pseudo Code: expect the function and argument to result in a number
 
-var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester2 = "academy"
-// Expected output: 0
-var vowelTester3 = "challenges"
-// Expected output: 2
-
+describe("vowelTester", () => {
+  var vowelTester1 = "learn";
+  // Expected output: 1
+  var vowelTester2 = "academy";
+  // Expected output: 0
+  var vowelTester3 = "challenges";
+  // Expected output: 2
+  it("logs the index of the first vowel in the string", () => {
+    expect(vowelTester(vowelTester1)).toEqual(1);
+    expect(vowelTester(vowelTester2)).toEqual(0);
+    expect(vowelTester(vowelTester3)).toEqual(2);
+  });
+});
+//RED     ReferenceError: vowelTester is not defined
 
 
 // b) Create the function that makes the test pass.
+// Pseudo Code: declare a function of vowelTester that takes in a string
+// use a for loop to itterate through the string and returns the index of the first vowel
+// use an if statement to deteremine if the letter at the current index is a vowel
+    // return the current index
+
+const vowelTester = (string) => {
+    let vowels = ["a","A","e","E","i","I","o","O","u","U"]
+    for (let i = 0; i < string.length; i++) {
+        if (vowels.includes(string[i]) === true) {
+          return i;
+        } 
+    }
+}
+// 2nd attempt with .search() - searches through the string and returns the index of the first item it finds.
+// Regular Expression - REGEX  /i  <-- Case Insensitive
+
+// indexOf() <-- returns the first index of the item in the argument
+
+// const vowelTester = (string) => {
+//  return string.search(/[aeiou]/i)
+// };;
--- interview-questions.md
+++ interview-questions.md
@@ -10,7 +10,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: "Parameter" is a placeholder for the data that the user passes information too in the function. The "Argument" is the data that is passed to the parameter. The "Parameter" represents data but does not contain any data itself. 
 
 
 
@@ -18,7 +18,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: The .map() function has five predefined parameters they are function, value, index, array, and thisValue. The two required parameters are function ()=>{} and value. 
 
 
 
@@ -26,23 +26,26 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
-
+  Researched answer: Object destructuring is the practice of assigning the different keys within an object to their own specific variables. Object destructering makes it possible to unpack the keys into an array. Object destructuring breaks down the value and creates a new variable and assigns them into a single assignment. An example of this might be a hyperlink in a word document. Or if we had a variable object car that has two keys color and brand, We could destructer the keys out of the object using the destructering syntax 
+  var { color, brand } = car 
+  var curly brace color, key curly brace equals car. 
+  
+  This would give me access to the values of color and brand. With this access I could reassign the color to be something different by just calling on color not by calling on car.color. 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: A method is something that preforms a task. A method is always associated with an object and a function is not associated with an object or a specific code block. Functions can manipulate objects but do not reside inside an object.  Functions are dependent on the information passed to them in the function invocation, whereas methods have access to the input information and the properties of the associated object where the method is being stored. This allows the developer to call on "this.key" to access any property in the object within the method alongside the methods parameters.
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: An object is a container for information. it contains methods and data. A class is a set of blueprints and instructions the object needs to follow upon instantiation. The behavior of the information inside the object is dictated by the class.  
 
 
 
@@ -50,7 +53,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: Before the execution of code interpreter moves the functions, variables, classes to the top of the scripts scope which allows the invocation of the function before it's even defined. You can also use classes before they are defined. Variables only get declare and not initialized. You can reassign a variable but not use the initial value before it's defined. 
 
 
 
@@ -58,12 +61,12 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance: A method of reusing functions from a parent class in a child class.
 
-2. React:
+2. React: A library of reusable and commonly used components
 
-3. React state:
+3. React state: An object used by react to represent information about the components current situation. 
 
-4. React lifecycle methods:
+4. React lifecycle methods: Each component in react has a life cycle with three phases that can be monitored. The phases are Mounting, Updating, and Unmounting. render(), componentDidMount(), componentDidUpdate(), componentWillUnmount(). 
 
-5. DOM:
+5. DOM: The Document Object Model is an interface for web documents. It represents the page so other programs can interact, change, and style the content.  
